c4d8b380fc3805733268fbf9325d28cd4ee12cd8
Lingvistik <emil1811@gmail.com>
2020-08-04 11:26:55 +0100

    Delete functionality added, chords showing on a separate page, css added, chords diagram added, preparation for edit functionality

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'songbook'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://Lingvistik:'+str(db_pass)+'@prviklaster-v3qci.mongodb.net/songbook?retryWrites=true&w=majority')
 
-
 mongo = PyMongo(app)
 
 @app.route('/')
@@ -21,6 +20,11 @@ def route():
 @app.route('/get_chords')
 def get_chords():
     return render_template('chordbook.html', chords=mongo.db.chords.find())
+
+@app.route('/show_chords/<chord_id>')
+def show_chords(chord_id):
+    the_chord = mongo.db.chords.find_one({"_id": ObjectId(chord_id)})
+    return render_template('showchords.html', chords=the_chord)
 
 @app.route('/add_chords')
 def add_chords():
@@ -34,4 +38,14 @@ def chords_diagram():
 def insert_chords():
     chords = mongo.db.chords
     chords.insert_one(request.form.to_dict())
-    return redirect(url_for('get_chords'))  
+    return redirect(url_for('get_chords')) 
+
+@app.route('/delete_chords/<chord_id>')
+def delete_chords(chord_id):
+    mongo.db.chords.remove({'_id': ObjectId(chord_id)})
+    return redirect(url_for('get_chords'))
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
